8436846fde3c9572b05fb6f4472419fad8c8aa43
TienHung <hunga13@gmail.com>
2016-05-07 09:34:51 +0700

    BTVN 4

---
+++ BTVN4/src/vn/edu/techkids/controllers/EnemyBulletController.java
@@ -0,0 +1,37 @@
+package vn.edu.techkids.controllers;
+
+
+import vn.edu.techkids.models.EnemyBullet;
+import vn.edu.techkids.models.GameConfig;
+import vn.edu.techkids.views.GameDrawer;
+
+/**
+ * Created by qhuydtvt on 5/6/2016.
+ */
+public class EnemyBulletController extends SingleController implements Colliable{
+
+    public EnemyBulletController(EnemyBullet gameObject, GameDrawer gameDrawer) {
+        super(gameObject, gameDrawer);
+        this.gameVector.dy = 5;
+        CollisionPool.getInst().add(this);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        if(!GameConfig.getInst().isInScreen(this.gameObject)) {
+            this.gameObject.setAlive(false);
+        }
+    }
+    @Override
+    public void onCollide (Colliable c) {
+        if (c instanceof PlaneController) {
+            c.getGameObject().HP--;
+            gameObject.setAlive(false);
+            if (c.getGameObject().HP == 0) {
+                c.getGameObject().setAlive(false);
+            }
+
+        }
+    }
+}
